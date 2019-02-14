58aed782e8058328c6305fa415f0dfbd563b7ab3
LAB-pesquisa <roger_amaros@hotmail.com>
2019-02-14 11:05:20 -0200

    140220191105

--- django_project/EduSite/EduSite/urls.py
+++ django_project/EduSite/EduSite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import inicio
+from .views import login 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inicio/', inicio),
+     path('login/', login),
 
 ]
--- django_project/EduSite/EduSite/views.py
+++ django_project/EduSite/EduSite/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request,"index.html")
+
+
+
+def login(request):
+    return render(request,"login.html")
