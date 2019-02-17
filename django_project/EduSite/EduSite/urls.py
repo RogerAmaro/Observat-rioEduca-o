a97dcc8120e0b7e18b9143744cdb49f9d0f800de
Roger Amaro <roger_amaros@hotmail.com>
2019-02-17 19:32:33 -0300

    170220191932

--- django_project/EduSite/EduSite/urls.py
+++ django_project/EduSite/EduSite/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import inicio
-from .views import login 
+from .views import login , cliente_login
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inicio/', inicio),
-    path('login/', login, name='login'),
-
-
+    path('login/', cliente_login, name='login'),
 ]
--- django_project/EduSite/EduSite/views.py
+++ django_project/EduSite/EduSite/views.py
@@ -1,13 +1,33 @@
-from django.http import HttpResponse
 
-from django.shortcuts import render_to_response, render
-from django.http import HttpResponseRedirect # Funcao para redirecionar o usuario
-from django.contrib.auth.forms import UserCreationForm # Formulario de criacao de usuarios
-from django.contrib.auth.forms import AuthenticationForm # Formulario de autenticacao de usuarios
-from django.contrib.auth import login # funcao que salva o usuario na sessao
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
 def inicio(request):
     return render(request,"index.html")
 
-def login(request):
-    return render(request,"login.html")
+# def login(request):
+#     return render(request,"login.html")
+
+
+def cliente_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'],
+                   password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated',
+                        'successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid Login')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
