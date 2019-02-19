6fc66320c9edb3f7eb8b77d3869f6dc10e1af604
LAB-pesquisa <roger_amaros@hotmail.com>
2019-02-19 17:48:02 -0300

    190220191747

--- django_project/env/bin/activate.fish
+++ django_project/env/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/lab-pesquisa/Desktop/Observat-rioEduca-o/django_project/env"
+set -gx VIRTUAL_ENV "/home/lab-pesquisa/Desktop/projetos/Observat-rioEduca-o/django_project/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- django_project/env/bin/django-admin.py
+++ django_project/env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/lab-pesquisa/Desktop/Observat-rioEduca-o/django_project/env/bin/python3
+#!/home/lab-pesquisa/Desktop/projetos/Observat-rioEduca-o/django_project/env/bin/python3
 from django.core import management
 
 if __name__ == "__main__":
